a3fa12f129a60348445302dddc35e704a01a6024
Stefan Cruz <17972991+stefancruz@users.noreply.github.com>
2024-05-03 23:37:10 +0100

    chore: .

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const crypto = require('crypto');
 const app = express();
 
 // Serve static files from the 'public' directory
-// app.use(express.static('public'));
+app.use(express.static('public'));
 
 // Define a route to serve index.html
 app.get('/', (req, res) => {
