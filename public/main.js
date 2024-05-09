c9516f66462959342e3f6cc4275a05ad23b9e555
Stefan Cruz <17972991+stefancruz@users.noreply.github.com>
2024-05-09 19:13:53 +0100

    wip

--- public/main.js
+++ public/main.js
@@ -3,12 +3,69 @@ $(document).ready(function () {
     const apiQrUrl = 'https://peertube-instances.ir-to.com/api/v1/PluginQR?peerTubePlatformUrl=';
 
 
-    
+
     const apiConfUrl = 'grayjay://plugin/https://peertube-instances.ir-to.com/api/v1/PluginConfig.json?peerTubePlatformUrl=';
     const peerTubeInstancesBaseUrl = 'https://instances.joinpeertube.org/api/v1/instances?start=0&count=1000&healthy=true&customizations=3&sort=-customizations&randomSortSeed=1714740'
 
     // Check if the browser supports custom URI scheme redirection
     var isSupported = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+    function toastError(title){
+        Swal.fire({
+            icon: 'error',
+            title,
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000
+        })
+    }
+
+    function openInGrayJay(host) {
+        window.location = `${apiConfUrl}${host}`;
+    }
+
+    function showQrCode(host) {
+
+        $('[data-toggle="tooltip"]').tooltip('hide');
+
+        var imageUrl = `${apiQrUrl}${host}`;
+
+        Swal.showLoading();
+
+        // Create an image element dynamically
+        var img = new Image();
+        img.onload = function () {
+            Swal.fire({
+                title: 'Scan to add to GrayJay',
+                imageUrl,
+                imageAlt: 'Generated QR Code',
+                html: `<p>QR code for ${host}:</p>`,
+                confirmButtonText: 'Open in grayjay',
+                showCancelButton: true,
+                showConfirmButton: isSupported,
+                cancelButtonText: 'Close',
+                buttonsStyling: false,
+                customClass: {
+                    actions: 'btn-group',
+                    confirmButton: 'btn btn-primary',
+                    cancelButton: 'btn btn-secondary'
+                }
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    openInGrayJay(host);
+                } else if (result.dismiss === Swal.DismissReason.cancel) {
+                    // Handle the "Cancel" button click
+                    // You can add your logic here
+                }
+            });
+        }
+
+
+        // Set the source of the image
+        img.src = `${apiQrUrl}${host}`;
+
+    }
 
     // Initialize DataTable
     $('#instancesTable').DataTable({
@@ -54,53 +111,60 @@ $(document).ready(function () {
         scrollY: false, // Disable vertical scrolling
         initComplete: function () {
             $('#instancesTable tbody').on('dblclick ', 'tr', function () {
-                $('[data-toggle="tooltip"]').tooltip('hide');
+                
                 var data = $('#instancesTable').DataTable().row(this).data();
-        
-                if (data.host) {
-                    // Show loading state
-                    Swal.showLoading();
-                    
-                    // Create an image element dynamically
-                    var img = new Image();
-                    img.onload = function() {
-                        // Once the image is loaded, create the SweetAlert2 alert
-                        Swal.fire({
-                            title: `Add ${data.host} to grayjay`,
-                            html: `
-                                <a href="https://grayjay.app/#download" target="_blank" rel="noopener">Download grayjay</a>
-                                </br>
-                                <img src="${apiQrUrl}${data.host}">
-                            `,
-                            confirmButtonText: 'Open in grayjay',
-                            showCancelButton: true,
-                            showConfirmButton: isSupported,
-                            cancelButtonText: 'Close',
-                            buttonsStyling: false,
-                            customClass: {
-                                actions: 'btn-group',
-                                confirmButton: 'btn btn-primary mb-2',
-                                cancelButton: 'btn btn-secondary mb-2'
-                            }
-                        }).then((result) => {
-                            if (result.isConfirmed) {
-                                window.location = `${apiConfUrl}${data.host}`;
-                            } else if (result.dismiss === Swal.DismissReason.cancel) {
-                                // Handle the "Cancel" button click
-                                // You can add your logic here
-                            }
-                        });
-                    };
-                    
-                    // Set the source of the image
-                    img.src = `${apiQrUrl}${data.host}`;
-                }
+
+                showQrCode(data.host);
+
             });
-        },        
+        },
         drawCallback: function () {
             $('[data-toggle="tooltip"]').tooltip();
         },
         responsive: true
 
     });
+
+    // Event listener for the generate QR code button
+    $('#generateQRBtn').click(function () {
+        Swal.fire({
+            title: 'Enter PeerTube Instance URL',
+            input: 'text',
+            inputPlaceholder: 'Enter instance URL',
+            showCancelButton: true,
+            confirmButtonText: 'Generate QR Code',
+            showLoaderOnConfirm: true,
+            inputValidator: (value) => {
+                // Check if a value is provided
+                if (!value) {
+                    return 'URL is required';
+                }
+            },
+            preConfirm: (url) => {
+                return new Promise((resolve, reject) => {
+                    const qrImage = new Image();
+                    qrImage.src = `${apiQrUrl}${url}`;
+                    qrImage.onload = () => {
+                        
+                        resolve({ url, qrImage });
+                    };
+                    qrImage.onerror = (error) => {
+                        
+                        reject('Failed to generate QR code. Please try again.');
+                    };
+                });
+            },
+            allowOutsideClick: () => !Swal.isLoading()
+        }).then((result) => {
+            
+            if (result.isConfirmed) {
+                const { url, qrImage } = result.value;
+                showQrCode(url);
+            }
+
+        }).catch(function(){
+            toastError('Invalid QR Code');
+        });
+    });
+
 });
